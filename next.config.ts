18d4e334147380d8b1b5a03d35288ccf588255d1
Chandrika Bandaru <149655493+chandrika-bandaru@users.noreply.github.com>
2026-01-31 21:18:03 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'export', // ← important: tells Next.js to do static export
+}
+
+export default nextConfig
+
+
